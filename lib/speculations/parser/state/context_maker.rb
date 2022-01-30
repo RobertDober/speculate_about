05da2fc59dfe4fab23446a842a5a9f8ed2f56605
Robert Dober <robert.dober@gmail.com>
2022-01-30 18:09:57 +0100

    Refactoring against Code Duplication

---
+++ lib/speculations/parser/state/context_maker.rb
@@ -0,0 +1,16 @@
+module Speculations
+  class Parser
+    module State
+      module ContextMaker
+        private
+
+        def make_new_context(lnb:, match:, node:)
+          level = match[:level].size
+          new_parent = node.parent_of_level(level.pred)
+          node = new_parent.new_context(title: match[:title], lnb: lnb, level: level)
+          [:out, node]
+        end
+      end
+    end
+  end
+end
--- lib/speculations/parser/state/includes.rb
+++ lib/speculations/parser/state/includes.rb
@@ -1,7 +1,9 @@
+require_relative 'context_maker'
 module Speculations
   class Parser
     module State
       module Includes extend self
+        include ContextMaker
 
         def parse line, lnb, node, _ctxt
           case
@@ -14,15 +16,6 @@ module Speculations
           else
             [:includes, node]
           end
-        end
-
-        private
-
-        def make_new_context(lnb:, match:, node:)
-          level = match[:level].size
-          new_parent = node.parent_of_level(level.pred)
-          node = new_parent.new_context(title: match[:title], lnb: lnb, level: level)
-          [:out, node]
         end
 
       end
