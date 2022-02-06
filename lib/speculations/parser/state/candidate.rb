c50c774e3bcc4a9814460ce47ba08707cce20d5b
Robert Dober <robert.dober@gmail.com>
2022-02-06 21:40:39 +0100

    Making Code Climate happy again

--- lib/speculations/parser/state/candidate.rb
+++ lib/speculations/parser/state/candidate.rb
@@ -7,26 +7,39 @@ module Speculations
           when State.blank_line(line)
             [:candidate, node, ctxt]
           when match = State.context_match(line)
-            level = match[1].size
-            new_parent = node.parent_of_level(level.pred)
-            node = new_parent.new_context(title: match[2], lnb: lnb, level: level)
-            [:out, node]
+            _parse_context(match, lnb:, node:)
           when match = State.maybe_include(line)
             [:candidate, node, :inc]
           when match = State.maybe_example(line)
             [:candidate, node, match[:title]]
           when match = State.ruby_code_block(line)
+            _parse_ruby_code_block(match, ctxt:, lnb:, node:)
+          else
+            [:out, node]
+          end
+        end
+
+        private
+
+        def _parse_context(match, lnb:, node:)
+          level = match[1].size
+          new_parent = node.parent_of_level(level.pred)
+          node = new_parent.new_context(title: match[2], lnb: lnb, level: level)
+          [:out, node]
+        end
+
+        def _parse_ruby_code_block(match, ctxt:, lnb:, node:)
             if ctxt == :inc
               node = node.new_include(lnb: lnb)
             else
               node = node.new_example(title: ctxt, lnb: lnb)
             end
             [:in, node, (ctxt == :inc ? :includes : :out)]
-          else
-            [:out, node]
-          end
         end
+
+
       end
     end
   end
 end
+#  SPDX-License-Identifier: Apache-2.0
